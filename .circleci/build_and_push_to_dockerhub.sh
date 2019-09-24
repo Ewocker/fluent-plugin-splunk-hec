d49d30e6f96b9a9e44639b7f6e4823a7ba08e648
Yu-An Lin <ylin@proofpoint.com>
2019-09-24 15:21:21 -0700

    update circle-ci for faking build

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -3,9 +3,9 @@ set -e
 echo "Building docker image..."
 cp /tmp/pkg/fluent-plugin-splunk-hec-*.gem docker
 VERSION=`cat VERSION`
-docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-splunk-hec:ci ./docker
-docker tag splunk/fluent-plugin-splunk-hec:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
+docker build --build-arg VERSION=$VERSION --no-cache -t ewocker/fluent-plugin-splunk-hec:ci ./docker
+docker tag ewocker/fluent-plugin-splunk-hec:ci ewocker/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
-docker push splunk/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
+docker push ewocker/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
 echo "Docker image pushed successfully to docker-hub."
